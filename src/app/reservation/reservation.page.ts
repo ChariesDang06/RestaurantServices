4941acc9b7b59df71d5cd6dce9de94c606795b22
Mucer72 <khanhlsfs@gmail.com>
2024-10-12 14:09:14 +0700

    first commit

--- src/app/reservation/reservation.page.ts
+++ src/app/reservation/reservation.page.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, NgModule, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-reservation',
@@ -8,8 +8,26 @@ import { Component, OnInit } from '@angular/core';
 export class ReservationPage implements OnInit {
 
   constructor() { }
-
   ngOnInit() {
   }
+  maps = [
+    { name: 'Sảnh chính', src: 'assets/images/image1.png' },
+    { name: 'Tầng 1', src: 'assets/images/image2.png' },
+    { name: 'Tầng 2', src: 'assets/images/image3.png' }
+  ];
 
+  tables = [
+    { name: 'ban 1', id: '1' },
+    { name: 'ban 2', id: '2' },
+    { name: 'ban 3', id: '3' }
+  ];
+
+  selectedMap = this.maps[0];
+  mapName=this.selectedMap.name;
+   
+
+  onMapChange(event: any) {
+    this.selectedMap = event.detail.value.src;
+  }
 }
+  
